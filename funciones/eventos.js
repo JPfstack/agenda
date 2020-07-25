281c1be4a8a816e235a8655afe9991223f478781
JPfstack <juanpedro.jimenez@gmail.com>
2020-07-25 20:53:37 +0200

    Mejoras de diseño.

--- funciones/eventos.js
+++ funciones/eventos.js
@@ -8,9 +8,6 @@ let btnEliminar = document.querySelector('#eliminar');
 let contadorTarea = 0;
 
 
-
-
-
 btnGuardar.addEventListener('click', capturarTarea);
 
 
@@ -21,19 +18,24 @@ function capturarTarea(event) {
     let prioridad = priTarea.value;
 
 
+
+
     const newTarea = {
         id: contadorTarea,
         tarea: tarea,
         prioridad: prioridad
     }
+
+
+
     guardarTarea(newTarea);
 
     pintarTarea(newTarea);
     inputTarea.value = "";
     contadorTarea++;
-   
- 
- 
+
+
+
 
 }
 
@@ -47,10 +49,9 @@ function pintarTarea(pTarea) {
 }
 
 
-
-
 function borrarTarea(pId) {
     let tareaBorrar = document.getElementById(pId);
+    tareaBorrar.setAttribute('style', 'transform: scale(2)')
     seccionTareas.removeChild(tareaBorrar);
     let id = parseInt(pId);
     posicionBorrar = listaTareas.findIndex(tarea => tarea.id == pId);
@@ -58,7 +59,6 @@ function borrarTarea(pId) {
 
 }
 
-let btnBuscarXPrioridad = document.querySelector('#buscar');
 let selectorPrioridad = document.querySelector('#selectorprioridad');
 selectorPrioridad.addEventListener('change', buscarXPrioridad);
 
@@ -84,16 +84,22 @@ inputBuscarTarea.addEventListener('input', capturarXInput);
 
 
 function capturarXInput(event) {
-    let inputBuscado = event.target.value;
+    let inputBuscado = event.target.value.toLowerCase();
     let resultadoBusqueda = buscarXInput(listaTareas, inputBuscado);
     pintarTareasXInput(resultadoBusqueda);
 
 }
 
-const listaXInputBuscado = new Array();
+
 
 function buscarXInput(pListaTareas, pInputBuscado) {
-    let listaXInputBuscado = pListaTareas.filter(tarea => tarea.tarea.includes(pInputBuscado));
+    let listaXInputBuscado = new Array();
+
+    listaXInputBuscado = pListaTareas.filter(tarea => {
+        let tareaMinusculas = tarea.tarea.toLowerCase();
+        return tareaMinusculas.includes(pInputBuscado);
+
+    });
     return listaXInputBuscado;
 
 }
@@ -103,46 +109,11 @@ function pintarTareasXInput(pListaXInput) {
     pListaXInput.forEach(tarea => { pintarTarea(tarea) });
 }
 
+let img = document.querySelector('#imagen');
 
-   
+function cambiarImg() {
+    let arrImg = new Array['img_1.jpg', 'img_2.jpg', 'img_3.jpg'];
+    img.src = arrImg[0];
 
-    
-
-
-
-/* function pintarUnaTarea(pTarea) {
-    seccionTareas.innerHTML += `<article data-id=${pTarea.id} id="tarea_${pTarea.id}">
-                                <h3>${pTarea.tarea}</h3>
-                                <p>${pTarea.prioridad}</p >
-                                <a onclick="borrarTarea('tarea_${pTarea.id}')" class="eliminar" >Eliminar</a>
-                                 </article >`
- */
-
-    /*    //crear los elementos
-       let article = document.createElement('article');
-       let h3 = document.createElement('h3');
-       let p = document.createElement('p');
-       let a = document.createElement('a');
-       a.className = "eliminar";
-   
-   
-       //creo los contenidos
-   
-       let contentH3 = document.createTextNode(pTarea.tarea);
-       let contentp = document.createTextNode(pTarea.prioridad);
-   
-       //tiro los contenidos dentro de los elementos.
-   
-   
-       h3.appendChild(contentH3);
-       p.appendChild(contentp);
-   
-   
-   
-       article.appendChild(h3);
-       article.appendChild(p);
-       article.appendChild(a);
-   
-       seccionTareas.appendChild(article); */
-
-
+}
+cambiarImg();
--- funciones/funciones.js
+++ funciones/funciones.js
@@ -4,12 +4,6 @@ function guardarTarea(pTarea) {
     listaTareas.push(pTarea);
 }
 
-/* function borrar(pId) {
-    let id = parseInt(pId);
-    let posicionBorrar = listaTareas.findIndex(tarea => tarea.id == pId);
-    listaTareas.splice(posicionBorrar, 1);
-    console.log(posicionBorrar);
-} */
 
 function filtrarXPrioridad(pListaTareas, pPrioridad) {
     let listaFiltradaXPrioridad = new Array();
